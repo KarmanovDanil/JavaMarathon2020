77022da729c6b9b7abd858237fcb4f86afa86f8a
karmanovdanil <karmanovdanil@gmail.com>
2022-04-26 18:23:50 +0500

    Java practice

---
+++ src/Day1/Task2.java
@@ -0,0 +1,13 @@
+package Day1;
+
+/**
+ * Повторить задание 1, но используя цикл for
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.print("JAVA ");
+        }
+    }
+}
